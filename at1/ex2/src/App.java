67d139218ada190acb7c98735bea7b5570a74454
Caio Tenório <caiomatenorio@gmail.com>
2025-03-15 16:08:18 -0300

    Add ex2 from at1

---
+++ at1/ex2/src/App.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var scanner = new Scanner(System.in);
+        double seconds, days, hours, minutes;
+
+        try {
+            seconds = askForSeconds(scanner);
+
+            days = calculateDays(seconds);
+            hours = calculateHours(seconds);
+            minutes = calculateMinutes(seconds);
+
+            System.out.printf("Dias: %f\n", days);
+            System.out.printf("Horas: %f\n", hours);
+            System.out.printf("Minutos: %f\n", minutes);
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static double askForSeconds(Scanner scanner) {
+        double seconds;
+
+        while (true) {
+            System.out.print("Insira um valor em segundos: ");
+
+            if (scanner.hasNextDouble()) {
+                seconds = scanner.nextDouble();
+
+                if (seconds >= 0)
+                    return seconds;
+            }
+
+            System.out.println("Valor inválido. Insira um número positivo.");
+            scanner.next();
+        }
+    }
+
+    public static double calculateDays(double seconds) {
+        return seconds / 86400;
+    }
+
+    public static double calculateHours(double seconds) {
+        return seconds / 3600;
+    }
+
+    public static double calculateMinutes(double seconds) {
+        return seconds / 60;
+    }
+}
