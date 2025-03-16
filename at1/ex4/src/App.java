8c4c24506ab4016a2987372d1c392807e2d7a005
Caio Tenório <caiomatenorio@gmail.com>
2025-03-16 19:11:54 -0300

    Add ex4 from at1

---
+++ at1/ex4/src/App.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int days;
+        double kilometers, diaryValue, additionalKilometerValue, totalValue;
+
+        var scanner = new Scanner(System.in);
+
+        try {
+            days = askForDays(scanner);
+            kilometers = askForKilometers(scanner);
+
+            diaryValue = calculateDiaryValue(days);
+            additionalKilometerValue = calculateAdditionalKilometerValue(kilometers);
+
+            totalValue = diaryValue + additionalKilometerValue;
+
+            System.out.printf("O valor total do aluguel foi R$%.2f.\n", totalValue);
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private static int askForDays(Scanner scanner) {
+        int days;
+
+        while (true) {
+            System.out.print("Insira o número de dias rodados: ");
+
+            if (scanner.hasNextInt()) {
+                days = scanner.nextInt();
+
+                if (days >= 0)
+                    return days;
+            }
+
+            System.out.println("Insira um valor inteiro positivo válido.");
+        }
+    }
+
+    private static double askForKilometers(Scanner scanner) {
+        double kilometers;
+
+        while (true) {
+            System.out.print("Insira o número de quilômetros rodados: ");
+
+            if (scanner.hasNextDouble()) {
+                kilometers = scanner.nextDouble();
+
+                if (kilometers >= 0)
+                    return kilometers;
+            }
+
+            System.out.println("Insira um valor numérico positivo válido.");
+        }
+    }
+
+    private static double calculateDiaryValue(int days) {
+        return days * 90;
+    }
+
+    private static double calculateAdditionalKilometerValue(double kilometers) {
+        if (kilometers <= 100)
+            return 0;
+
+        return (kilometers - 100) * 12;
+    }
+}
